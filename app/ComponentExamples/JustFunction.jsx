b487e706ea1d741205c489ef90283314d81159dc
Elbookyes <80248189+ElBookyes@users.noreply.github.com>
2026-04-30 00:18:17 +0300

    dice roll page

---
+++ app/ComponentExamples/JustFunction.jsx
@@ -0,0 +1,4 @@
+export function JustFunction() {
+    
+    return <div>Hello</div>
+}
---
+++ app/ComponentExamples/SomeComponent.jsx
@@ -0,0 +1,10 @@
+import { JustFunction } from "./JustFunction";
+
+export function SomeComponent() {
+    return <>
+    <div>
+        <h1>We are using this component</h1>
+        <JustFunction />
+    </div>
+    </>
+}
